cb1079d21d98e36d43f43ea74b755abaff75be27
Frost7994 <tommybyrne9@gmail.com>
2024-01-19 10:32:05 +0000

    refactor(app): refactored folder structure and added some basic styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,52 @@
 import type { Config } from "tailwindcss";
 
+// colors
+import colors from "tailwindcss/colors";
+const flattenColorPalette =
+  require("tailwindcss/lib/util/flattenColorPalette").default;
+
+const addVariablesForColors = ({
+  addBase,
+  theme,
+}: {
+  addBase: (base: any) => void;
+  theme: (path: string) => any;
+}) => {
+  let allColors = flattenColorPalette(theme("colors"));
+
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+};
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: colors.sky,
+        secondary: colors.fuchsia,
+        tertiary: {
+          ...colors.stone,
+          950: "#141210",
+          1000: "#0C0A09",
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("tailwind-scrollbar"),
+    require("@tailwindcss/container-queries"),
+    addVariablesForColors,
+  ],
 };
 export default config;
